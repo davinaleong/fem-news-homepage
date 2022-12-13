af7dfabf00836057946db989057bda67001756bc
Davina Leong <davina.leong.sy@gmail.com>
2022-12-13 16:15:24 +0800

    Done with header

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 console.log(`main.js loaded.`)
+
+// Variables
+const dataOpenAttr = `data-open`
+
+const primaryHeaderEl = document.querySelector(
+  `[data-element="primary-header"]`
+)
+const primaryHeaderToggleMenuEl = document.querySelector(
+  `[data-element="primary-header-menu-icon"]`
+)
+const primaryHeaderToggleCloseEl = document.querySelector(
+  `[data-element="primary-header-close-icon"]`
+)
+
+primaryHeaderToggleMenuEl.addEventListener(`click`, (event) => {
+  primaryHeaderEl.setAttribute(dataOpenAttr, true)
+})
+
+primaryHeaderToggleCloseEl.addEventListener(`click`, (event) => {
+  primaryHeaderEl.removeAttribute(dataOpenAttr)
+})
